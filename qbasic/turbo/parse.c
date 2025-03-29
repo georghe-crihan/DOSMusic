5dc4a69128ebd768f3975ae7ffdae0a76917277e
Gheorghe Crihan <gheorghe.crihan@shrapnel.com>
2025-03-29 14:09:30 -0700

    - Portable Turbo Basic string segment parser.

--- qbasic/turbo/parse.c
+++ qbasic/turbo/parse.c
@@ -6,29 +6,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned short seg = 0xC020;
-unsigned short descr = 0xB420;
-unsigned short descr_end = 0xBF82;
+/* String segment */
+u_int16_t seg = 0xC020;
+/* Scalar segment: String Descriptor array */
+u_int16_t descr = 0xB420;
+u_int16_t descr_end = 0xBF82;
 
 typedef struct __attribute__((packed)) {
-  unsigned short len;
-  unsigned short off;
+  u_int16_t len;
+  u_int16_t off;
 } strdescr;
 
 int main(int argc, char **argv)
 {
 FILE *infile = NULL;
 strdescr *dp = (strdescr *)malloc(descr_end-descr);
-char *s = (char *)malloc(64000);
+char *s = (char *)malloc(65536);
 
 infile = fopen(argv[1], "rb");
 fseek(infile, (long)descr, SEEK_SET);
 fread(dp, 1, descr_end-descr, infile);
-printf("Descriptor length: %d\n", descr_end-descr);
+printf("Descriptor array length: %d\n", descr_end-descr);
 for (strdescr *p = dp; p < dp + descr_end-descr; ++p) {
     fseek(infile, (long) seg + p->off, SEEK_SET);
-    unsigned short l = (p->len) & ~0x8000;
-    printf("Length: %d\n", l); 
+    /* The high bit might or might not be set, just mask it out */
+    u_int16_t l = (p->len) & ~0x8000;
     fread(s, 1, l, infile);
     s[l] = '\0';
     printf("Length: %d, string: %s\n", l, s); 
