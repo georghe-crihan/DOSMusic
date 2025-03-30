c149b04436ff88603b030fdaf4245f5631c9ff40
Gheorghe Crihan <gheorghe.crihan@shrapnel.com>
2025-03-29 18:14:39 -0700

    - Better Watcom source.

--- dem137.c
+++ dem137.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <dos.h>
 
-volatile int clock_ticks;
+typedef unsigned char u_int8_t;
+typedef unsigned short u_int16_t;
+typedef short int16_t;
+
+volatile u_int8_t flag1; 
+volatile u_int8_t flag2;
+volatile u_int16_t cnt1;
+volatile u_int16_t cnt1_max;
+
 void (__interrupt __far *prev_int_1c)();
 
 void __interrupt __far timer_rtn()
   {
-  unsigned __int8 v0; // al
-  unsigned __int16 v1; // bx
-  unsigned __int8 v2; // al
-  unsigned __int8 v3; // al
-  __int16 v4; // bx
+  u_int8_t v0; // al
+  u_int16_t v1; // bx
+  u_int8_t v2; // al
+  u_int8_t v3; // al
+  int16_t v4; // bx
+  u_int16_t vt;
 
   _disable();
   if ( flag1 != -1 )
@@ -45,13 +54,13 @@ void __interrupt __far timer_rtn()
       {
         vt = 0x1234DDu / v1;
         __outbyte(0x42u, vt);
-        __outbyte(0x42u, (unsigned __int16)vt >> 8);
+        __outbyte(0x42u, (u_int16_t)vt >> 8);
         v2 = __inbyte(0x61u);
         __outbyte(0x61u, v2 | 3);
       }
       cnt1 = 0;
       v4 = idx1 + 2;
-      if ( (__int16)(idx1 + 2) > /* idx1_max */word_1018F )
+      if ( (int16_t)(idx1 + 2) > /* idx1_max */word_1018F )
         v4 = 0;
       idx1 = v4;
     }
