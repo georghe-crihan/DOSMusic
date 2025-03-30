367d75cd3997422d5cb7021eba0089b1934e08c8
Gheorghe Crihan <gheorghe.crihan@shrapnel.com>
2025-03-29 17:14:54 -0700

    - Make the Convertor more Python-like thing.

--- qbasic/play2mid.py
+++ qbasic/play2mid.py
@@ -1,13 +1,13 @@
-def WriteVarLen(v: int) -> str:
+def write_var_len(v: int) -> str:
     # a: str
-    a=chr(v AND 127)
+    a=chr(v and 127)
 
     while v > 127:
         v = v >> 7
-        a=chr((v AND 127) OR 128) + a
+        a=chr((v and 127) or 128) + a
     return a
 
-def WriteFourBytes(v: int) -> str:
+def write_four_bytes(v: int) -> str:
     # a: str
     a=chr(v and 255)
     v >>= 8
@@ -19,28 +19,30 @@ def WriteFourBytes(v: int) -> str:
     return a
 
 xlatNote = { # (n: str) -> ubyte:
-        "c": 0,
-	"cs": 1, "db": 1,
-	"d": 2,
-	"ds": 3, "eb": 3,
-	"e": 4, "fb" : 4,
-	"f": 5, "es" : 5,
-	"fs": 6, "gb": 6,
-	"g": 7,
-	"gs": 8, "ab" 8,
-	"a": 9,
-	"as": 10, "bb": 10,
-	"b": 11, "cb": 11
-        }
+    "c": 0,
+    "cs": 1, "db": 1,
+    "d": 2,
+    "ds": 3, "eb": 3,
+    "e": 4, "fb" : 4,
+    "f": 5, "es" : 5,
+    "fs": 6, "gb": 6,
+    "g": 7,
+    "gs": 8, "ab" 8,
+    "a": 9,
+    "as": 10, "bb": 10,
+    "b": 11, "cb": 11
+    }
 
 
 def _fbplay_internal(channel: ubyte, playstr: str) -> str:
-	# default tempo is 120 quarter notes per minute
-	# default note is a quarter note
-	# as default notes play their full length
-	# default octave is the 4th
-	# default instrument is acoustic grand piano |TODO: Find a instrument closer to QB's PLAY sound.
-	# maximum volume is default
+    '''
+	default tempo is 120 quarter notes per minute
+	default note is a quarter note
+	as default notes play their full length
+	default octave is the 4th
+	default instrument is acoustic grand piano |TODO: Find a instrument closer to QB's PLAY sound.
+	maximum volume is default
+	'''
 
 	# Track: str
 
@@ -51,25 +53,25 @@ def _fbplay_internal(channel: ubyte, playstr: str) -> str:
 	volume:  ubyte = 127
 	note_stack[128]: ubyte
 
-	chord: ubyte
-	next_event: double
+	#chord: ubyte
+	#next_event: double
 
-	duration: double
-	idx: ubyte
+	#duration: double
+	#idx: ubyte
        
-	number: str
-	ch:  str #*1
-	tChar: str #*1
+	#number: str
+	#ch:  char
+	#tChar: char
        
-	toTranslate: str
+	#toTranslate: str
 
-	p: int=1
+	p = 1 #: int
        
 	while p < len(playstr):
 		ch=playstr[p].lower()
 		p+=1
 
-			# basic playing
+		# basic playing
 			if ch == "n":      # plays note with next-comming number, if 0 then pause
 				number=""
 				while True:
@@ -79,14 +81,14 @@ def _fbplay_internal(channel: ubyte, playstr: str) -> str:
 						number+=tchar
 					else:
 						break
-				idx=val(number)
+				idx=int(number)
 
 				if idx==0: # pause
 					next_event+=60/tempo*(4/note_len)/60
 				else: # note
 					duration=60/tempo*(4/note_len)
 
-					Track=Track+WriteVarLen(240*next_event)+chr(&H90 + channel)+chr(idx)+chr(volume)
+					Track=Track+write_var_len(240*next_event)+chr(&H90 + channel)+chr(idx)+chr(volume)
 
 					next_event=duration*(1-note_len_mod)
 					#stop_note(channel)=t+duration*note_len_mod(channel)
@@ -117,12 +119,12 @@ def _fbplay_internal(channel: ubyte, playstr: str) -> str:
 						number+=ch
 					else:
 						break
-				if val(number)<>0: duration=duration*4/val(number)
+				if int(number)<>0: duration=duration*4/int(number)
 				if ch==".": duration=duration*1.5
 
 				idx=12*octave+xlatNote[toTranslate]
 
-				Track=Track+WriteVarLen(240*next_event)+chr(&H90 + channel)+chr(idx)+chr(volume)
+				Track=Track+write_var_len(240*next_event)+chr(&H90 + channel)+chr(idx)+chr(volume)
 
 				next_event=duration*(1-note_len_mod)
 
@@ -139,7 +141,7 @@ def _fbplay_internal(channel: ubyte, playstr: str) -> str:
 						number+=ch
 					else:
 						break
-				next_event+=60/tempo*4/val(number)
+				next_event+=60/tempo*4/int(number)
 		         
 		   
 			# octave handling
@@ -158,7 +160,7 @@ def _fbplay_internal(channel: ubyte, playstr: str) -> str:
 						number+=ch
 					else:
 						break
-				octave=val(number)
+				octave=int(number)
 		         
 		         
 			# play control
@@ -171,7 +173,7 @@ def _fbplay_internal(channel: ubyte, playstr: str) -> str:
 						number+=ch
 					else:
 				            break	
-				tempo=val(number)
+				tempo=int(number)
 
 			elif ch == "l":      # changes note length (1=full note, 4=quarter note, 8 eigth(?) note aso)
 				number=""
@@ -182,7 +184,7 @@ def _fbplay_internal(channel: ubyte, playstr: str) -> str:
 						number+=ch
 					else:
                                             break
-				note_len=val(number)
+				note_len=int(number)
 		     
 			elif ch == "m":      # MS makes note last 3/4, MN is 7/8 and ML sets to normal length
 				ch=playstr[p].lower()
@@ -202,7 +204,7 @@ def _fbplay_internal(channel: ubyte, playstr: str) -> str:
 						number+=ch
 					else:
 						break
-				Track=Track+WriteVarLen(0)+chr(&HC0 + channel)+chr(val(number))
+				Track=Track+write_var_len(0)+chr(&HC0 + channel)+chr(int(number))
 		     
 			elif ch == "v":
 				number=""
@@ -213,7 +215,7 @@ def _fbplay_internal(channel: ubyte, playstr: str) -> str:
 						number+=ch
 					else:
 						break
-				volume=val(number)
+				volume=int(number)
 			elif ch == "{":      # enable chords (notes play simultaneously)
 				chord=1
 			Case "}"      # disable chords (notes play simultaneously)
@@ -225,7 +227,7 @@ def _fbplay_internal(channel: ubyte, playstr: str) -> str:
 		else:
 			# Stop current note, if still playing
 			for i in range(1, note_stack[0]):
-				Track=Track+WriteVarLen(240*duration*note_len_mod)+chr(&H80 + channel)+chr(note_stack[i])+chr(0)
+				Track=Track+write_var_len(240*duration*note_len_mod)+chr(&H80 + channel)+chr(note_stack[i])+chr(0)
 				duration=0
 			note_stack[0]=0
 
@@ -246,72 +248,71 @@ def play(playstr: str, playstr1: str="", playstr2: str="", playstr3: str="",
 	Track: str
 	Track=_fbplay_internal(0,playstr)
 	if len(Track)>0:
-		Midi=Midi +"MTrk"+WriteFourBytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
+		Midi=Midi +"MTrk"+write_four_bytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
 		Tracks+=1
 	Track=_fbplay_internal(1,playstr1)
 	if len(Track)>0: 
-		Midi=Midi +"MTrk"+WriteFourBytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
+		Midi=Midi +"MTrk"+write_four_bytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
 		Tracks+=1
 	Track=_fbplay_internal(2,playstr2)
 	if len(Track)>0:
-		Midi=Midi +"MTrk"+WriteFourBytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
+		Midi=Midi +"MTrk"+write_four_bytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
 		Tracks+=1
 	Track=_fbplay_internal(3,playstr3)
 	if len(Track)>0: 
-		Midi=Midi +"MTrk"+WriteFourBytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
+		Midi=Midi +"MTrk"+write_four_bytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
 		Tracks+=1
 	Track=_fbplay_internal(4,playstr4)
 	if len(Track)>0:
-		Midi=Midi +"MTrk"+WriteFourBytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
+		Midi=Midi +"MTrk"+write_four_bytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
 		Tracks+=1
 	Track=_fbplay_internal(5,playstr5)
 	if len(Track)>0:
-		Midi=Midi +"MTrk"+WriteFourBytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
+		Midi=Midi +"MTrk"+write_four_bytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
 		Tracks+=1
 	Track=_fbplay_internal(6,playstr6)
 	if len(Track)>0:
-		Midi=Midi +"MTrk"+WriteFourBytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
+		Midi=Midi +"MTrk"+write_four_bytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
 		Tracks+=1
 	Track=_fbplay_internal(7,playstr7)
 	if len(Track)>0:
-		Midi=Midi +"MTrk"+WriteFourBytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
+		Midi=Midi +"MTrk"+write_four_bytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
 		Tracks+=1
 	Track=_fbplay_internal(8,playstr8)
 	if len(Track)>0: 
-		Midi=Midi +"MTrk"+WriteFourBytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
+		Midi=Midi +"MTrk"+write_four_bytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
 		Tracks+=1
 	Track=_fbplay_internal(9,playstr9)
 	if len(Track)>0:
-		Midi=Midi +"MTrk"+WriteFourBytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
+		Midi=Midi +"MTrk"+write_four_bytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
 		Tracks+=1
 	Track=_fbplay_internal(10,playstr10)
 	if len(Track)>0:
-		Midi=Midi +"MTrk"+WriteFourBytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
+		Midi=Midi +"MTrk"+write_four_bytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
 		Tracks+=1
-	end if
 	Track=_fbplay_internal(11,playstr11)
 	if len(Track)>0:
-		Midi=Midi +"MTrk"+WriteFourBytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
+		Midi=Midi +"MTrk"+write_four_bytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
 		Tracks+=1
 	Track=_fbplay_internal(12,playstr12)
 	if len(Track)>0:
-		Midi=Midi +"MTrk"+WriteFourBytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
+		Midi=Midi +"MTrk"+write_four_bytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
 		Tracks+=1
 	Track=_fbplay_internal(13,playstr13)
 	if len(Track)>0:
-		Midi=Midi +"MTrk"+WriteFourBytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
+		Midi=Midi +"MTrk"+write_four_bytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
 		Tracks+=1
 	Track=_fbplay_internal(14,playstr14)
 	if len(Track)>0: 
-		Midi=Midi +"MTrk"+WriteFourBytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
+		Midi=Midi +"MTrk"+write_four_bytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
 		Tracks+=1
 	Track=_fbplay_internal(15,playstr15)
 	if len(Track)>0: 
-		Midi=Midi +"MTrk"+WriteFourBytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
+		Midi=Midi +"MTrk"+write_four_bytes(len(Track)+4)+Track+chr(0)+chr(255)+chr(47)+chr(0)
 		Tracks+=1
 
 	with open("output.mid", "wa") as output: 
 	    output.write("MThd"+chr(0)+chr(0)+chr(0)+chr(6)+chr(0)+chr(iif(Tracks>1,1,0))+chr(0)+chr(Tracks)+chr(0)+chr(120)+Midi)
 
 
-PLAY " i48 t200l4mneel2el4eel2el4egl3cl8dl1el4ffl3fl8fl4fel2el8eel4eddel2dgl4eel2el4eel2el4egl3cl8dl1el4ffl3fl8fl4fel2el8efl4ggfdl2cl8"
+play(" i48 t200l4mneel2el4eel2el4egl3cl8dl1el4ffl3fl8fl4fel2el8eel4eddel2dgl4eel2el4eel2el4egl3cl8dl1el4ffl3fl8fl4fel2el8efl4ggfdl2cl8")
